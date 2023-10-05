406c0dcf87c440197f8a3ebc81a2cfa1a544e135
Randy23123 <rlopez@appdev.yearup.org>
2023-10-05 13:34:20 -0400

    adding multi

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Whats your name?: ");
+        String name = scanner.nextLine();
+
+        System.out.println("How many hours did you work today?: ");
+        Float num1 = scanner.nextFloat();
+
+        System.out.println("How much do you get paid?: ");
+        Float num2 = scanner.nextFloat();
+
+        
+
+
+
+    }
 }
